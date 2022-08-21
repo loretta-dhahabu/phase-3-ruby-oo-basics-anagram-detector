7f0e2c5464a6dbb47a26d5f81cc1c92b50d87632
loretta dhahabu <loreydjefwa@gmail.com>
2022-08-21 08:19:22 +0300

    commit tree 71350bec73e805533d06a11e46875356074b81e1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+#require "pry"
+class Anagram
+    def initialize (word)
+        @word=word
+    end
+
+    def match (word_list)
+        match=[]
+        word_list.each do |item|
+            if @word.split("").sort == item.split("").sort
+                match.push(item)
+            end
+        end
+        match
+    end
+end
